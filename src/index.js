7b0dd99143907918773efdbe9ae132f7bc5fc9ea
Mark Strange <strange.mark88@gmail.com>
2019-09-12 14:47:01 -0500

    add delete button

--- src/index.js
+++ src/index.js
@@ -22,44 +22,70 @@ const {getMovies} = require('./api.js');
 // });
 
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
+////////////// Load movies from json ////////////////////////////
 
-  $('#loader').css('display', 'none');
 
-  console.log(movies);
-  console.log(movies.length);
-  let idCount = movies.length;
+function loadMovies(){
+  getMovies().then((movies) => {
+    console.log('Here are all the movies:');
+    console.log(movies);
+    $('#loader').css('display', 'none');
 
-  let movieList = '';
+    let movieList = '';
 
-      movies.forEach(({title, rating}) => {
-        movieList += `<div> - ${title} - rating: ${rating}</div>`;
+    movies.forEach(({title, rating, id}) => {
+      movieList += `<div class="${id}"> - ${title} - rating: ${rating}<button class="${id}">delete</button></div>`;
 
-      });
+    });
 
-          $('#movieOutput').html(movieList);
+    console.log(movieList);
 
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
+    $('#movieOutput').html(movieList);
 
-function addNewMovie() {
+    $('button').click(function() {
+      $(this).parent().css('display', 'none')
+    });
 
+
+  }).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
+  });
 }
 
-const newMovieEntry = {title: 'test', rating: '5'};
-const url = '/api/movies';
-const options = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(newMovieEntry),
-};
-fetch(url, options)
-    .then(getMovies())
-    .catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-});
+loadMovies();
+
+////////////////////     Add Movies     /////////////////////////////////////////////////////////////////
+
+$('#submitBTN').click(addNewMovie);
+
+function addNewMovie(e) {
+  e.preventDefault();
+
+  let newTitle = $('#add-new-title').val();
+
+  let newRating = $('.add-new-rating').val();
+
+
+  const newMovieEntry = {title: newTitle, rating: newRating};
+  const url = '/api/movies';
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newMovieEntry),
+  };
+  fetch(url, options)
+      .then(loadMovies())
+      .catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+  });
+}
+
+//////////////////////    Delete Movies  /////////////////////////////////////////////////
+
+
+// $('button').click(function() {
+//   $(this).css('display', 'none')
+// });
