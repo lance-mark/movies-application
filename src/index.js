04fc1d376a8cf62b9b1f6b3a3da2d503f7ec1967
Mark Strange <strange.mark88@gmail.com>
2019-09-12 10:24:49 -0500

    add loading

--- src/index.js
+++ src/index.js
@@ -4,16 +4,38 @@
 import sayHello from './hello';
 sayHello('World');
 
+const $ = require('./jquery-2.2.4');
+
 /**
  * require style imports
  */
 const {getMovies} = require('./api.js');
 
+// getMovies().then((movies) => {
+//   console.log('Here are all the movies:');
+//   movies.forEach(({title, rating, id}) => {
+//     console.log(`id#${id} - ${title} - rating: ${rating}`);
+//   });
+// }).catch((error) => {
+//   alert('Oh no! Something went wrong.\nCheck the console for details.')
+//   console.log(error);
+// });
+
+
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
+
+  $('#loader').css('display', 'none');
+
+    let movieList = '';
+
+      movies.forEach(({title, rating}) => {
+        movieList += `<div> - ${title} - rating: ${rating}</div>`;
+
+      });
+
+          $('#movieOutput').html(movieList);
+
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
